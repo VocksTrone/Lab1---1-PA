a54f801dc8670b051d4846dcf3d4622878c3c618
VocksTrone <jr.alvarez0316@gmail.com>
2024-07-29 09:18:24 -0600

    Agregar archivos de proyecto.

---
+++ Lab1 - 1; PA/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+    public Product(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+    public void Sell(int quantity)
+    {
+        if (Quantity >= quantity)
+        {
+            Quantity -= quantity;
+            Console.WriteLine($"Se vendieron {quantity} productos {Name}.");
+            Console.WriteLine($"Aún hay {Quantity} Productos con este Nombre");
+        }
+    }
+}
